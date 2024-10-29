6d882f719fd40ffe17b3a069f280c5b6c80b04ce
Alexander Semenov <sasfeat@mail.ru>
2024-10-29 21:30:07 +0100

    Fix paths uncertainty

--- sw.js
+++ sw.js
@@ -1,26 +1,59 @@
-const isGithubPages = window.location.hostname.includes('github.io');
-const isSkyFire = window.location.hostname.includes('skyfirestudio.com');
-const basePath = isGithubPages ? '/demo-offline-map/' : (isSkyFire ? '/maps/' : '/');
-
-
-cacheAssets = [
-    basePath,
-    `${basePath}index.html`,
-    `${basePath}libs/leaflet.css`,
-    `${basePath}libs/leaflet.js`,
-    `${basePath}images/marker-icon.png`,
-    `${basePath}images/marker-shadow.png`,
-    `${basePath}images/marker-icon-2x.png`
-];
 const cacheName = 'maptiler-raster-cache-v3';
+let basePath = '/'; // Default value
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
+        self.skipWaiting(), // Skip waiting to activate the service worker immediately
         caches.open(cacheName).then(function(cache) {
             return cache.addAll(cacheAssets);
         })
     );
 });
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        clients.claim(), // Claim clients to start controlling them immediately
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(existingCacheName) {
+                    if (existingCacheName !== cacheName) {
+                        console.log('Deleting old cache:', existingCacheName);
+                        return caches.delete(existingCacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('message', function(event) {
+    if (event.data && event.data.basePath) {
+        basePath = event.data.basePath;
+        updateCacheAssets();
+    }
+});
+
+function updateCacheAssets() {
+    cacheAssets = [
+        basePath,
+        `${basePath}index.html`,
+        `${basePath}libs/leaflet.css`,
+        `${basePath}libs/leaflet.js`,
+        `${basePath}images/marker-icon.png`,
+        `${basePath}images/marker-shadow.png`,
+        `${basePath}images/marker-icon-2x.png`
+    ];
+}
+
+let cacheAssets = [
+    '/',  // Default assets, will be updated once basePath is received
+    '/index.html',
+    '/libs/leaflet.css',
+    '/libs/leaflet.js',
+    '/images/marker-icon.png',
+    '/images/marker-shadow.png',
+    '/images/marker-icon-2x.png'
+];
 
 self.addEventListener('fetch', function(event) {
     if (event.request.url.includes('https://api.maptiler.com/maps/streets-v2/')) {
@@ -60,19 +93,3 @@ self.addEventListener('fetch', function(event) {
         );
     }
 });
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(existingCacheName) {
-                    if (existingCacheName !== cacheName) {
-                        console.log('Deleting old cache:', existingCacheName);
-                        return caches.delete(existingCacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
