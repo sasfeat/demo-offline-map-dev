8e11a21774c4ab45468be8480692c2a8ff185e74
Alexander Semenov <sasfeat@mail.ru>
2024-10-29 20:50:36 +0100

    Delete old cache

--- sw.js
+++ sw.js
@@ -53,3 +53,19 @@ self.addEventListener('fetch', function(event) {
         );
     }
 });
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(existingCacheName) {
+                    if (existingCacheName !== cacheName) {
+                        console.log('Deleting old cache:', existingCacheName);
+                        return caches.delete(existingCacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
